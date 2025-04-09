0a892f9c0ea0558772a61a158e00464bae196c8d
Jay Bosamiya <jaybosamiya@gmail.com>
2025-04-09 16:53:32 -0700

    safe locals

--- doom-emacs/.config/doom/custom.el
+++ doom-emacs/.config/doom/custom.el
@@ -1,1 +1,8 @@
 (put 'customize-group 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((rustic-rustfmt-args . "--edition 2021") (bibtex-align-at-equal-sign . t))))
